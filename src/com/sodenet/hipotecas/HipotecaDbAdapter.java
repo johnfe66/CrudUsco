15401225983557647f18eba90327ef69f62baf2a
Johnfe Vargas <johnfe66@gmail.com>
2016-09-20 16:34:05 -0500

    Initial commit

---
+++ src/com/sodenet/hipotecas/HipotecaDbAdapter.java
@@ -0,0 +1,189 @@
+package com.sodenet.hipotecas;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.CursorLoader;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+
+public class HipotecaDbAdapter {
+
+	//
+	// Definimos constante con el nombre de la tabla
+	//
+	public static final String C_TABLA = "HIPOTECA" ;
+	
+    //
+    // Definimos constantes con el nombre de las columnas de la tabla
+    //
+    public static final String C_COLUMNA_ID	= "_id";
+    public static final String C_COLUMNA_NOMBRE = "hip_nombre";
+    public static final String C_COLUMNA_CONDICIONES = "hip_condiciones";
+    public static final String C_COLUMNA_CONTACTO = "hip_contacto";
+    public static final String C_COLUMNA_EMAIL = "hip_email";
+    public static final String C_COLUMNA_TELEFONO = "hip_telefono";
+    public static final String C_COLUMNA_OBSERVACIONES = "hip_observaciones";
+    public static final String C_COLUMNA_PASIVO = "hip_pasivo_sn";
+    public static final String C_COLUMNA_SITUACION = "hip_sit_id";
+
+
+    private Context contexto;
+    private HipotecaDbHelper dbHelper;
+    private SQLiteDatabase db;
+
+    //
+    // Definimos lista de columnas de la tabla para utilizarla en las consultas a la base de datos
+    //
+    private String[] columnas = new String[]{
+            C_COLUMNA_ID,
+            C_COLUMNA_NOMBRE,
+            C_COLUMNA_CONDICIONES,
+            C_COLUMNA_CONTACTO,
+            C_COLUMNA_EMAIL,
+            C_COLUMNA_TELEFONO,
+            C_COLUMNA_OBSERVACIONES,
+            C_COLUMNA_PASIVO,
+            C_COLUMNA_SITUACION} ;
+
+	public HipotecaDbAdapter(Context context)
+	{
+		this.contexto = context;
+	}
+
+	public HipotecaDbAdapter abrir() throws SQLException
+	{
+		dbHelper = new HipotecaDbHelper(contexto);
+		db = dbHelper.getWritableDatabase();
+		return this;
+	}
+
+	public void cerrar()
+	{
+		dbHelper.close();
+	}
+
+	
+    /**
+     * Devuelve cursor con todos los registros y columnas de la tabla
+     */
+    public Cursor getCursor(String filtro) throws SQLException {
+
+        if (db == null)
+            abrir();
+
+        Cursor c = db.query(true, C_TABLA, columnas, filtro, null, null, null, null, null);
+
+        return c;
+    }
+	
+	/**
+	 * Devuelve cursor con todos las columnas del registro
+	 */
+	public Cursor getRegistro(long id) throws SQLException
+	{
+        if (db == null)
+            abrir();
+
+        Cursor c = db.query( true, C_TABLA, columnas, C_COLUMNA_ID + "=" + id, null, null, null, null, null);
+		
+		//Nos movemos al primer registro de la consulta
+		if (c != null) {
+			c.moveToFirst();
+		}
+		return c;
+	}
+	
+	/**
+	 * Inserta los valores en un registro de la tabla
+	 */
+	public long insert(ContentValues reg)
+	{
+		if (db == null)
+			abrir();
+		
+		return db.insert(C_TABLA, null, reg);
+	}
+	
+	/**
+	 * Eliminar el registro con el identificador indicado
+	 */
+	public long delete(long id)
+	{
+		if (db == null)
+			abrir();
+		
+		return db.delete(C_TABLA, "_id=" + id, null);
+	}
+	
+    /**
+     * Modificar el registro
+     */
+    public long update(ContentValues reg)
+    {
+        long result = 0;
+
+        if (db == null)
+            abrir();
+
+        if (reg.containsKey(C_COLUMNA_ID))
+        {
+            //
+            // Obtenemos el id y lo borramos de los valores
+            //
+            long id = reg.getAsLong(C_COLUMNA_ID);
+
+            reg.remove(C_COLUMNA_ID);
+
+            //
+            // Actualizamos el registro con el identificador que hemos extraido
+            //
+            result = db.update(C_TABLA, reg, "_id=" + id, null);
+        }
+        return result;
+    }
+
+    /**
+     * Comprueba si existe el registro
+     */
+    public boolean exists(long id) throws SQLException
+    {
+        boolean exists ;
+
+        if (db == null)
+            abrir();
+
+        Cursor c = db.query( true, C_TABLA, columnas, C_COLUMNA_ID + "=" + id, null, null, null, null, null);
+
+        exists = (c.getCount() > 0);
+
+        c.close();
+
+        return exists;
+    }
+
+    public ArrayList<Hipoteca> getHipotecas(String filtro)
+    {
+        ArrayList<Hipoteca> hipotecas = new ArrayList<Hipoteca>();
+
+        if (db == null)
+            abrir();
+
+        Cursor c = db.query(true, C_TABLA, columnas, filtro, null, null, null, null, null);
+
+        for(c.moveToFirst(); !c.isAfterLast(); c.moveToNext())
+        {
+            hipotecas.add(Hipoteca.cursorToHipoteca(contexto, c));
+        }
+
+        c.close();
+
+        return hipotecas;
+    }
+
+
+
+}
---
+++ src/com/sodenet/hipotecas/HipotecaFormulario.java
@@ -0,0 +1,284 @@
+package com.sodenet.hipotecas;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class HipotecaFormulario extends Activity {
+	
+	private SituacionDbAdapter dbAdapterSituacion ;
+    private SituacionSpinnerAdapter situacionSpinnerAdapter ;
+
+	//
+    // Modo del formulario
+    //
+	private int modo ;
+	
+	//
+	// Identificador del registro que se edita cuando la opción es MODIFICAR
+	//
+	private long id ;
+    private Hipoteca hipoteca = new Hipoteca(this);
+	
+    //
+    // Elementos de la vista
+    //
+    private EditText nombre;
+    private EditText condiciones;
+    private EditText contacto;
+    private EditText telefono;
+    private EditText email;
+    private EditText observaciones;
+    private CheckBox pasivo ;
+    private Spinner situacion ;
+	
+	private Button boton_guardar;
+	private Button boton_cancelar;
+
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_hipoteca_formulario);
+		
+		Intent intent = getIntent();
+		Bundle extra = intent.getExtras();
+
+		if (extra == null) return;
+		
+        //
+        // Obtenemos los elementos de la vista
+        //
+        nombre = (EditText) findViewById(R.id.nombre);
+        condiciones = (EditText) findViewById(R.id.condiciones);
+        contacto = (EditText) findViewById(R.id.contacto);
+        telefono = (EditText) findViewById(R.id.telefono);
+        email = (EditText) findViewById(R.id.email);
+        observaciones = (EditText) findViewById(R.id.observaciones);
+        pasivo = (CheckBox) findViewById(R.id.pasivo);
+        situacion = (Spinner) findViewById(R.id.situacion);
+
+		boton_guardar = (Button) findViewById(R.id.boton_guardar);
+		boton_cancelar = (Button) findViewById(R.id.boton_cancelar);
+
+        //
+        // Creamos el adaptador del spinner de situaciones y lo asociamos
+        //
+        situacionSpinnerAdapter = new SituacionSpinnerAdapter(this, Situacion.getAll(this, null));
+        situacion.setAdapter(situacionSpinnerAdapter);
+
+		//
+		// Obtenemos el identificador del registro si viene indicado
+		//
+		if (extra.containsKey(HipotecaDbAdapter.C_COLUMNA_ID))
+		{
+			id = extra.getLong(HipotecaDbAdapter.C_COLUMNA_ID);
+			consultar(id);
+		}
+		
+		//
+		// Establecemos el modo del formulario
+		//
+		establecerModo(extra.getInt(HipotecaActivity.C_MODO));
+		
+		//
+		// Definimos las acciones para los dos botones
+		//
+		boton_guardar.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v)
+			{
+				guardar();
+			}
+		});
+		
+		boton_cancelar.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v)
+			{
+				cancelar();	
+			}
+		});
+		
+	}
+	
+    private void establecerModo(int m)
+    {
+        this.modo = m ;
+
+        if (modo == HipotecaActivity.C_VISUALIZAR)
+        {
+            this.setTitle(nombre.getText().toString());
+            this.setEdicion(false);
+        }
+        else if (modo == HipotecaActivity.C_CREAR)
+        {
+            this.setTitle(R.string.hipoteca_crear_titulo);
+            this.setEdicion(true);
+        }
+        else if (modo == HipotecaActivity.C_EDITAR)
+        {
+            this.setTitle(R.string.hipoteca_editar_titulo);
+            this.setEdicion(true);
+        }
+    }
+	
+private void consultar(long id)
+{
+    //
+    // Consultamos la hipoteca por el identificador
+    //
+    hipoteca = Hipoteca.find(this, id);
+
+    nombre.setText(hipoteca.getNombre());
+    condiciones.setText(hipoteca.getCondiciones());
+    contacto.setText(hipoteca.getContacto());
+    telefono.setText(hipoteca.getTelefono());
+    email.setText(hipoteca.getEmail());
+    observaciones.setText(hipoteca.getObservaciones());
+    pasivo.setChecked(hipoteca.isPasivo());
+    situacion.setSelection(situacionSpinnerAdapter.getPositionById(hipoteca.getSituacionId()));
+
+}
+
+private void setEdicion(boolean opcion)
+{
+    nombre.setEnabled(opcion);
+    condiciones.setEnabled(opcion);
+    contacto.setEnabled(opcion);
+    telefono.setEnabled(opcion);
+    email.setEnabled(opcion);
+    observaciones.setEnabled(opcion);
+    pasivo.setEnabled(opcion);
+    situacion.setEnabled(opcion);
+
+    // Controlamos visibilidad de botonera
+    LinearLayout v = (LinearLayout) findViewById(R.id.botonera);
+
+    if (opcion)
+        v.setVisibility(View.VISIBLE);
+
+    else
+        v.setVisibility(View.GONE);
+}
+	
+private void guardar()
+{
+    hipoteca.setNombre(nombre.getText().toString());
+    hipoteca.setCondiciones(condiciones.getText().toString());
+    hipoteca.setContacto(contacto.getText().toString());
+    hipoteca.setTelefono(telefono.getText().toString());
+    hipoteca.setEmail(email.getText().toString());
+    hipoteca.setObservaciones(observaciones.getText().toString());
+    hipoteca.setPasivo(pasivo.isChecked());
+    hipoteca.setSituacionId(situacion.getSelectedItemId());
+
+    hipoteca.save();
+
+    if (modo == HipotecaActivity.C_CREAR)
+    {
+        Toast.makeText(HipotecaFormulario.this, R.string.hipoteca_crear_confirmacion, Toast.LENGTH_SHORT).show();
+    }
+    else if (modo == HipotecaActivity.C_EDITAR)
+    {
+        Toast.makeText(HipotecaFormulario.this, R.string.hipoteca_editar_confirmacion, Toast.LENGTH_SHORT).show();
+    }
+
+    //
+    // Devolvemos el control
+    //
+    setResult(RESULT_OK);
+    finish();
+}
+	
+	private void cancelar()
+	{
+		setResult(RESULT_CANCELED, null);
+		finish();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		menu.clear();
+	    
+		if (modo == HipotecaActivity.C_VISUALIZAR)
+			getMenuInflater().inflate(R.menu.hipoteca_formulario_ver, menu);
+		
+		else
+			getMenuInflater().inflate(R.menu.hipoteca_formulario_editar, menu);
+		
+		return true;
+	}
+	
+@Override
+public boolean onMenuItemSelected(int featureId, MenuItem item) {
+	
+	switch (item.getItemId())
+	{
+		case R.id.menu_eliminar:
+			borrar(id);
+			return true;
+			
+		case R.id.menu_cancelar:
+			cancelar();
+			return true;
+			
+		case R.id.menu_guardar:
+			guardar();
+			return true;
+			
+		case R.id.menu_editar:
+			establecerModo(HipotecaActivity.C_EDITAR);
+			return true;
+	}
+	
+	return super.onMenuItemSelected(featureId, item);
+}
+	
+	private void borrar(final long id)
+	{
+		/*
+		 * Borramos el registro con confirmación
+		 */
+		AlertDialog.Builder dialogEliminar = new AlertDialog.Builder(this);
+		
+		dialogEliminar.setIcon(android.R.drawable.ic_dialog_alert);
+		dialogEliminar.setTitle(getResources().getString(R.string.hipoteca_eliminar_titulo));
+		dialogEliminar.setMessage(getResources().getString(R.string.hipoteca_eliminar_mensaje));
+		dialogEliminar.setCancelable(false);
+		
+		dialogEliminar.setPositiveButton(getResources().getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+
+			public void onClick(DialogInterface dialog, int boton) {
+				hipoteca.delete();
+				Toast.makeText(HipotecaFormulario.this, R.string.hipoteca_eliminar_confirmacion, Toast.LENGTH_SHORT).show();
+				/*
+				 * Devolvemos el control
+				 */
+				setResult(RESULT_OK);
+				finish();
+			}
+		});
+		
+		dialogEliminar.setNegativeButton(android.R.string.no, null);
+		
+		dialogEliminar.show();
+		
+	}
+
+}
